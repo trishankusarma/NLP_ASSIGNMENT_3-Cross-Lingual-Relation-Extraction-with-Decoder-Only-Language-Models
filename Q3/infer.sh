dc8e96a39bfb59aa589510775adc79bc78c2e7e7
trishankusarma <trishankusarma165@gmail.com>
2026-05-03 22:44:08 +0530

    Fixed Q3/infer.sh

--- Q3/infer.sh
+++ Q3/infer.sh
@@ -5,8 +5,6 @@ TEST_FILE=$2
 OUTPUT_DIR=${3:-"Q3/output"}
 echo "Task 3 ICL Inference | lang=$LANG | test=$TEST_FILE | out=$OUTPUT_DIR"
 
-module load compiler/gcc/11.2.0
-export LD_LIBRARY_PATH=/home/scai/msr/aiy247541/.conda/envs/vllm_server_nlp/lib/python3.10/site-packages/nvidia/cuda_runtime/lib:$LD_LIBRARY_PATH
 export CUDA_VISIBLE_DEVICES=0
 
 cd "$(dirname "$0")"
